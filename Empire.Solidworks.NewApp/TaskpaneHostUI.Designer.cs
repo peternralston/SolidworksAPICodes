b6dca0a6be693907722f8a167157d5cbb52e0e29
Peter Ralston <peternralston@gmail.com>
2023-08-16 14:21:36 -0500

    updating icon .png location and retrieval info

--- Empire.Solidworks.NewApp/TaskpaneHostUI.Designer.cs
+++ Empire.Solidworks.NewApp/TaskpaneHostUI.Designer.cs
@@ -28,32 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(169, 18);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(66, 28);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "My Button";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // TaskpaneHostUI
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.button1);
-            this.Name = "TaskpaneHostUI";
-            this.Size = new System.Drawing.Size(238, 193);
-            this.ResumeLayout(false);
-
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
