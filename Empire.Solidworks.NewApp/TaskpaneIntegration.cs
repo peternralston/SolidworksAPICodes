1a7bd1bea474164fb6a5854d68736e8c1bd473fb
Peter Ralston <peternralston@gmail.com>
2023-08-23 14:53:21 -0500

    fixed errors in code on TaskpanIntegration.cs

--- Empire.Solidworks.NewApp/TaskpaneIntegration.cs
+++ Empire.Solidworks.NewApp/TaskpaneIntegration.cs
@@ -15,7 +15,7 @@ namespace Empire.Solidworks.NewApp
     /// </summary>
     public class TaskpaneIntegration : SwAddin
     {
-        #region Privat Members
+        #region Private Members
 
         /// <summary>
         /// The cookie to the current instance of SolidWorks we are running inside of
@@ -23,33 +23,32 @@ namespace Empire.Solidworks.NewApp
         private int mSwCookie;
 
         /// <summary>
-        /// The taskpane vewi for our add-in
+        /// The taskpane view for our add-in
         /// </summary>
         private TaskpaneView mTaskpaneView;
 
         /// <summary>
-        /// The UI control that is going to be inside the Solidworks taskpane view
+        /// The UI control that is going to be inside the SolidWorks taskpane view
         /// </summary>
-
         private TaskpaneHostUI mTaskpaneHost;
 
         /// <summary>
         /// The current instance of the SolidWorks application
         /// </summary>
-        private SldWorks mSolidworksApplication;
-
+        private SldWorks mSolidWorksApplication;
 
         #endregion
 
         #region Public Members
+
         /// <summary>
-        /// The unique Id to the taskpane used for registeration in COM
+        /// The unique Id to the taskpane used for registration in COM
         /// </summary>
-        public const string SWTASKPANE_PROGID = "Empire.Solidworks.NewApp.Taskpane";
+        public const string SWTASKPANE_PROGID = "AngelSix.SolidWorks.BlankAddin.Taskpane";
 
         #endregion
 
-        #region Solidworks Add-in Callbacks
+        #region SolidWorks Add-in Callbacks
 
         /// <summary>
         /// Called when SolidWorks has loaded our add-in and wants us to do our connection logic
@@ -57,38 +56,34 @@ namespace Empire.Solidworks.NewApp
         /// <param name="ThisSW">The current SolidWorks instance</param>
         /// <param name="Cookie">The current SolidWorks cookie Id</param>
         /// <returns></returns>
-        /// <exception cref="NotImplementedException"></exception>
         public bool ConnectToSW(object ThisSW, int Cookie)
         {
             // Store a reference to the current SolidWorks instance
-            mSolidworksApplication = (SldWorks)ThisSW;
+            mSolidWorksApplication = (SldWorks)ThisSW;
 
             // Store cookie Id
             mSwCookie = Cookie;
 
-            // Setup Callback info
-            var ok = mSolidworksApplication.SetAddinCallbackInfo2(0, this, mSwCookie);
+            // Setup callback info
+            var ok = mSolidWorksApplication.SetAddinCallbackInfo2(0, this, mSwCookie);
 
             // Create our UI
-            LoadUi();
+            LoadUI();
 
-            // Return OK
+            // Return ok
             return true;
-
         }
-
 
         /// <summary>
         /// Called when SolidWorks is about to unload our add-in and wants us to do our disconnection logic
         /// </summary>
         /// <returns></returns>
-        /// <exception cref="NotImplementedException"></exception>
         public bool DisconnectFromSW()
         {
             // Clean up our UI
             UnloadUI();
 
-            // Return OK
+            // Return ok
             return true;
         }
 
@@ -99,27 +94,29 @@ namespace Empire.Solidworks.NewApp
         /// <summary>
         /// Create our Taskpane and inject our host UI
         /// </summary>
-        private void LoadUi()
+        private void LoadUI()
         {
-            // Find location to our taskpan icon
-            var imagePath = Path.Combine(Path.GetDirectoryName(typeof(TaskpaneIntegration).Assembly.CodeBase).Replace(@"file:\", string.Empty), "SolidworksAppBrain.png");
+            // Find location to our taskpane icon
+            var imagePath = Path.Combine(Path.GetDirectoryName(typeof(TaskpaneIntegration).Assembly.CodeBase).Replace(@"file:\", string.Empty), "logo-small.bmp");
 
             // Create our Taskpane
-            mSolidworksApplication.CreateTaskpaneView2(imagePath, "Woo, My first SqAddin");
-            // @"C:\Program Files\Repo\SolidworksAPICodes\Referances\SolidworksAppBrain.png" in case we need to hard code the location
+            mTaskpaneView = mSolidWorksApplication.CreateTaskpaneView2(imagePath, "Woo! My first SwAddin");
 
             // Load our UI into the taskpane
             mTaskpaneHost = (TaskpaneHostUI)mTaskpaneView.AddControl(TaskpaneIntegration.SWTASKPANE_PROGID, string.Empty);
         }
 
+        /// <summary>
+        /// Cleanup the taskpane when we disconnect/unload
+        /// </summary>
         private void UnloadUI()
         {
             mTaskpaneHost = null;
 
-            //Remove taskpane view
+            // Remove taskpane view
             mTaskpaneView.DeleteView();
 
-            // Release COM Reference and cleanup memory
+            // Release COM reference and cleanup memory
             Marshal.ReleaseComObject(mTaskpaneView);
 
             mTaskpaneView = null;
@@ -130,14 +127,13 @@ namespace Empire.Solidworks.NewApp
         #region COM Registration
 
         /// <summary>
-        /// The COM Registration call to add our registry entries to the SolidWorks add-in registry
+        /// The COM registration call to add our registry entries to the SolidWorks add-in registry
         /// </summary>
         /// <param name="t"></param>
-
         [ComRegisterFunction()]
         private static void ComRegister(Type t)
         {
-            var keyPath = string.Format($"SOFTWARE/SolidWorks/AddIns/{0:b}", t.GUID);
+            var keyPath = string.Format(@"SOFTWARE\SolidWorks\AddIns\{0:b}", t.GUID);
 
             // Create our registry folder for the add-in
             using (var rk = Microsoft.Win32.Registry.LocalMachine.CreateSubKey(keyPath))
@@ -145,7 +141,7 @@ namespace Empire.Solidworks.NewApp
                 // Load add-in when SolidWorks opens
                 rk.SetValue(null, 1);
 
-                // Set Solidworks add-in title and description 
+                // Set SolidWorks add-in title and description
                 rk.SetValue("Title", "My SwAddin");
                 rk.SetValue("Description", "All your pixels are belong to us!");
             }
@@ -158,11 +154,13 @@ namespace Empire.Solidworks.NewApp
         [ComUnregisterFunction()]
         private static void ComUnregister(Type t)
         {
-            var keyPath = string.Format(@"SOFTWARE/sOLIDwORKS/aDDiNS/{0:B}", t.GUID);
+            var keyPath = string.Format(@"SOFTWARE\SolidWorks\AddIns\{0:b}", t.GUID);
 
             // Remove our registry entry
             Microsoft.Win32.Registry.LocalMachine.DeleteSubKeyTree(keyPath);
+
         }
+
         #endregion
     }
 }
